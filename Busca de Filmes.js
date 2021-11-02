1a35f0f6c50400cd8f916e69e997365536c84b4e
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-11-02 00:33:58 -0300

    Adicionando pequenas funcionalidades e refatoração

--- Busca de Filmes.js
+++ Busca de Filmes.js
@@ -1,21 +1,27 @@
-function adicionarFilme(){
-    var campoFilmeFavorito = document.querySelector('#filme')
-    var filmeFavorito = campoFilmeFavorito.value//Tirando as tags do HTML e exibindo apenas o há escrito dentro do campo de endereço da imagem
-    
-    //Se a variável filmeFavorito for do tipo texto e terminar com outro texto que tenha a extensão .jpg, ou seja, se o endereço digitado do filme no campo de "endereço de imagem" tiver extensão .jpg, o filme é exibido
-    if (filmeFavorito.endsWith('.jpg')){
-      listarFilmesNaTela(filmeFavorito)
-    }
-    //Se terminar com outro tipo de extensão (que não seja .jpg) de arquivos de imagens, o filme não é encontrado
-    else{
-      alert("Imagem não encontrada! Digite apenas imagens que tenham extensão .jpg/ Exemplo: https://m.media-amazon.com/images/M/MV5BMTk0NzcxMjYwNF5BMl5BanBnXkFtZTcwMTI4MTIxMw@@._V1_UX182_CR0,0,182,268_AL_.jpg")
-    }
-    //Atribui um valor 0 (null) para o campo de pesquisa de filme, como se fosse um efeito de "limpar" o quê é escrito dentro do campo de pesquisa para pesquisar o filme favorito
-    campoFilmeFavorito.value = ""
+function adicionarFilme() {
+  var campoFilmeFavorito = document.querySelector('#filme')
+  var filmeFavorito = campoFilmeFavorito.value//Tirando as tags do HTML e exibindo apenas o há escrito dentro do campo de endereço da imagem
+
+  //Se a variável filmeFavorito for do tipo texto e terminar com outro texto que tenha a extensão .jpg
+  if (filmeFavorito.endsWith('.jpg')) {
+    listarFilmesNaTela(filmeFavorito)
   }
-  
-  function listarFilmesNaTela(filme){
-    var listarFilmes = document.querySelector('#listaFilmes')
-    var elementoFilme = "<img src=" + filme + ">"
-    listarFilmes.innerHTML = listarFilmes.innerHTML + elementoFilme
+  //Se terminar com outro tipo de extensão (que não seja .jpg), o filme não é exibido
+  else {
+    alert("Imagem não encontrada! Digite apenas imagens que tenham o link com a extensão .jpg/ Exemplo: https://m.media-amazon.com/images/M/MV5BMTk0NzcxMjYwNF5BMl5BanBnXkFtZTcwMTI4MTIxMw@@._V1_UX182_CR0,0,182,268_AL_.jpg")
   }
+
+  campoFilmeFavorito.value = ""
+}
+
+//Listando os filmes na tela
+function listarFilmesNaTela(filme) {
+  var listarFilmes = document.querySelector('#listarFilmes')
+  var elementoFilme = "<img src=" + filme + ">"
+  listarFilmes.innerHTML = listarFilmes.innerHTML + elementoFilme
+}
+
+const btnRetornaTopo = document.querySelector('.return-topo')
+window.addEventListener("scroll", () => {
+  scrollY >= 450 ? btnRetornaTopo.classList.add("show") : btnRetornaTopo.classList.remove("show")
+})
